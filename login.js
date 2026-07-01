30ac6b5ad1f9cd147879b584c301e28a6588f260
MathFreak72 <121536141+MathFreak72@users.noreply.github.com>
2026-07-02 01:13:25 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,3 +1,3 @@
-const PASSWORD='birthday2026';
+const PASSWORD='nakshajith';
 
 function login(){if(document.getElementById('pw').value===PASSWORD){sessionStorage.setItem('birthdayAuth','yes');location.href='birthday.html';}else document.getElementById('err').textContent='Incorrect password';}
